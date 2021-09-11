61e5cddb39408e1e6ac79dbe500eeb037e71cfb3
Javier Granados <jjgh89@msn.com>
2021-09-11 17:58:55 -0500

    Mode arvanced project on PC

--- hello.py
+++ hello.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, make_response, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     return '<h1>Hola Mundo</h1>'
+
+# Index storing a cookie
+# @app.route('/')
+# def index():
+#     response = make_response('<h1>This document carries a cookie</h1>')
+#     response.set_cookie('answer', '42')
+#     return response
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello {0} </h1>'.format(name)
